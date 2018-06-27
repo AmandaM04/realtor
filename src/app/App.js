f713425fda0155c81443dc9177960a5e5c6f4b00
Amanda Mitchell <wreedamitchell@gmail.com>
2018-06-26 19:20:15 -0500

    main components built

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings';
+import Building from '../components/Building/Building';
+import ListingForm from '../components/ListingForm/ListingForm';
 
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <Listings />
+      <div className="App">
+        <div className="col-sm-6">
+          <Listings />
+        </div>
+        <div className="col-sm-6">
+          <Building />
+        </div>
+        <div className="col-sm-12">
+          <ListingForm />
+        </div>
+      </div>
     );
   }
 }
